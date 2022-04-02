8492173981013a99c3f30f2165af5d52f283ce54
AleksTimofeev <timoha144a@gmail.com>
2022-04-03 00:07:28 +0300

    Updates

---
+++ precache-manifest.58889977d82f9dedc69db6b8cfd2ccb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d427116561da18d9d58f17130ca33770",
+    "url": "/homeworks-by-ignat/index.html"
+  },
+  {
+    "revision": "ff1511af2278d35271e9",
+    "url": "/homeworks-by-ignat/static/css/main.a1572a99.chunk.css"
+  },
+  {
+    "revision": "bc6d3350911711364505",
+    "url": "/homeworks-by-ignat/static/js/2.a06463af.chunk.js"
+  },
+  {
+    "revision": "248fe9facfff898840212404b4cb88d9",
+    "url": "/homeworks-by-ignat/static/js/2.a06463af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff1511af2278d35271e9",
+    "url": "/homeworks-by-ignat/static/js/main.c4c7d886.chunk.js"
+  },
+  {
+    "revision": "d6824a243215fa3682ff",
+    "url": "/homeworks-by-ignat/static/js/runtime-main.e13e2a19.js"
+  }
+]);
