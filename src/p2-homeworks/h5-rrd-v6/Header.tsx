5d8ad645f64222afe5b784f9bfe9ec7ffb7857e1
AleksTimofeev <timoha144a@gmail.com>
2022-04-02 23:56:06 +0300

    HW 05

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,12 +1,36 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
+import {PATH} from "./Pages";
+import styles from './HW5.module.css'
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
 
-        </div>
-    )
+
+
+  return (
+    <div className={styles.header}>
+    <div className={styles.navigation}>
+      <NavLink
+        className={({isActive}) => isActive ? styles.activeLink : ''}
+        to={PATH.PRE_JUNIOR}>
+        PreJunior
+        <span className={styles.borderLink}></span>
+      </NavLink>
+      <NavLink
+        className={({isActive}) => isActive ? styles.activeLink : ''}
+        to={PATH.JUNIOR}>
+        Junior
+        <span className={styles.borderLink}></span>
+      </NavLink>
+      <NavLink
+        className={({isActive}) => isActive ? styles.activeLink : ''}
+        to={PATH.JUNIOR_PLUS}>
+        Junior +
+        <span className={styles.borderLink}></span>
+      </NavLink>
+    </div>
+    </div>
+  )
 }
 
 export default Header
---
+++ src/p2-homeworks/h5-rrd-v6/pages/JuniorPlus.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const JuniorPlus = () => {
+  return (
+    <div>
+      <h1>JuniorPlus page</h1>
+    </div>
+  );
+};
+
+export default JuniorPlus;
