e05977062ecd5c92df17597ee56b9abe6218d6ae
AleksTimofeev <timoha144a@gmail.com>
2022-04-05 16:44:08 +0300

    HW 05

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,31 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {NavLink} from 'react-router-dom'
 import {PATH} from "./Pages";
 import styles from './HW5.module.css'
 
 function Header() {
 
-
+  let [showMenu, setShowMenu] = useState(false)
 
   return (
-    <div className={styles.header}>
-    <div className={styles.navigation}>
-      <NavLink
-        className={({isActive}) => isActive ? styles.activeLink : ''}
-        to={PATH.PRE_JUNIOR}>
-        PreJunior
-      </NavLink>
-      <NavLink
-        className={({isActive}) => isActive ? styles.activeLink : ''}
-        to={PATH.JUNIOR}>
-        Junior
-      </NavLink>
-      <NavLink
-        className={({isActive}) => isActive ? styles.activeLink : ''}
-        to={PATH.JUNIOR_PLUS}>
-        Junior +
-      </NavLink>
-    </div>
+    <div onMouseLeave={() => setShowMenu(false)} className={styles.header}>
+      <div className={`${styles.navigation} ${showMenu ? styles.showNavigation : styles.closeNavigation}`}>
+        <NavLink
+          className={({isActive}) => isActive ? styles.activeLink : ''}
+          to={PATH.PRE_JUNIOR}>
+          PreJunior
+        </NavLink>
+        <NavLink
+          className={({isActive}) => isActive ? styles.activeLink : ''}
+          to={PATH.JUNIOR}>
+          Junior
+        </NavLink>
+        <NavLink
+          className={({isActive}) => isActive ? styles.activeLink : ''}
+          to={PATH.JUNIOR_PLUS}>
+          Junior +
+        </NavLink>
+      </div>
+      <div
+        onClick={() => setShowMenu(!showMenu)}
+        className={styles.menu}>Nav</div>
     </div>
   )
 }
