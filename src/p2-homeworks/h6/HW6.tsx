3f45317a5d5bf01954ed30d75cd97cca799b0f81
AleksTimofeev <timoha144a@gmail.com>
2022-04-09 22:28:09 +0300

    HW 06

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -2,39 +2,58 @@ import React, {useState} from 'react'
 import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 import {restoreState, saveState} from './localStorage/localStorage'
+import styles from './HW6.module.css'
 
 function HW6() {
-    const [value, setValue] = useState<string>('')
+  const [value, setValue] = useState<string>('')
+  const [inputFocus, setInputFocus] = useState(false)
 
-    const save = () => {
-        saveState<string>('editable-span-value', value)
+  const save = () => {
+    if (value.trim() && value !=='enter text...') {
+      saveState<string>('editable-span-value', value)
     }
-    const restore = () => {
-        // setValue()
-    }
+  }
+  const callbackOnFocus = () => {
+    setInputFocus(true)
 
-    return (
-        <div>
-            <hr/>
-            homeworks 6
+  }
+  const callbackOnBlur = () => {
+    setInputFocus(false)
+  }
 
-            {/*should work (должно работать)*/}
-            <div>
-                <SuperEditableSpan
-                    value={value}
-                    onChangeText={setValue}
-                    spanProps={{children: value ? undefined : 'enter text...'}}
-                />
-            </div>
-            <SuperButton onClick={save}>save</SuperButton>
-            <SuperButton onClick={restore}>restore</SuperButton>
+  const restore = () => {
+    setValue(restoreState('editable-span-value', 'enter text...'))
+  }
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperEditableSpan/>*/}
-            <hr/>
-        </div>
-    )
+  return (
+    <div>
+      <hr/>
+      homeworks 6
+      <div className={styles.wrapper}>
+        <>
+          <SuperEditableSpan
+            value={value}
+            onChangeText={setValue}
+            onEnter={callbackOnBlur}
+            onFocus={callbackOnFocus}
+            onBlur={callbackOnBlur}
+            spanProps={{children: value ? undefined : 'enter text...'}}
+          />
+        </>
+        <div className={styles.buttons}>
+        {inputFocus ?
+          <SuperButton onClick={callbackOnBlur}>Add</SuperButton> :
+          <SuperButton disabled={!Boolean(value)} onClick={save}>save</SuperButton>
+        }
+        <SuperButton onClick={restore}>restore</SuperButton>
+      </div>
+      </div>
+      <hr/>
+      {/*для личного творчества, могу проверить*/}
+      {/*<AlternativeSuperEditableSpan/>*/}
+      <hr/>
+    </div>
+  )
 }
 
 export default HW6
