1516b1b614d7fbacb278a31c6231e1947db38af5
AleksTimofeev <timoha144a@gmail.com>
2022-05-09 23:33:34 +0300

    HW 10

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -1,38 +1,54 @@
 import React from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import {useDispatch, useSelector} from "react-redux";
+import {stateLoading} from "./bll/store";
+import {loadingAC} from "./bll/loadingReducer";
+import styles from './HW10.module.css'
 
 function HW10() {
-    // useSelector, useDispatch
-    const loading = false
+  const dispatch = useDispatch()
+  const loading: boolean = useSelector(stateLoading).loading
 
-    const setLoading = () => {
-        // dispatch
-        // setTimeout
-        console.log('loading...')
-    };
+  const setLoading = () => {
+    dispatch(loadingAC(true))
+    setTimeout(() => {
+      dispatch(loadingAC(false))
+    }, 3000)
+    console.log('loading...')
 
-    return (
-        <div>
-            <hr/>
-            homeworks 10
+  };
 
-            {/*should work (должно работать)*/}
-            {loading
-                ? (
-                    <div>крутилка...</div>
-                ) : (
-                    <div>
-                        <SuperButton onClick={setLoading}>set loading...</SuperButton>
-                    </div>
-                )
-            }
+  return (
+    <div className={styles.wrapper}>
+      <hr/>
+      homeworks 10
+      {loading
+        ? (
+          <div className={styles.center}>
+            <div className={styles.wave}></div>
+            <div className={styles.wave}></div>
+            <div className={styles.wave}></div>
+            <div className={styles.wave}></div>
+            <div className={styles.wave}></div>
+            <div className={styles.wave}></div>
+            <div className={styles.wave}></div>
+            <div className={styles.wave}></div>
+            <div className={styles.wave}></div>
+            <div className={styles.wave}></div>
+          </div>
+        ) : (
+          <div>
+            <SuperButton onClick={setLoading}>set loading...</SuperButton>
+          </div>
+        )
+      }
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<Alternative/>*/}
-            <hr/>
-        </div>
-    )
+      <hr/>
+      {/*для личного творчества, могу проверить*/}
+      {/*<Alternative/>*/}
+      <hr/>
+    </div>
+  )
 }
 
 export default HW10
--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,21 @@
-const initState = {
-
+export type InitialState = {
+  loading: boolean
+}
+const initState: InitialState = {
+  loading: false
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        case '': {
-            return state
-        }
-        default: return state
+export const loadingReducer = (state = initState, action: LoadingACType): InitialState => { // fix any
+  switch (action.type) {
+    case 'IS_LOADING': {
+      return {...state, loading: action.loading}
     }
+    default:
+      return state
+  }
 }
-
-export const loadingAC = (): any => {} // fix any
+type LoadingACType = {
+  type: 'IS_LOADING'
+  loading: boolean
+}
+export const loadingAC = (loading: boolean): LoadingACType => ({type: 'IS_LOADING', loading}) // fix any
